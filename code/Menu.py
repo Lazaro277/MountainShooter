3918d307243fe837fadfdd6a135b5b09e6314bfe
Lázaro <lazaromessias101@gmail.com>
2026-05-31 18:49:27 -0300

    changing menu

--- code/Menu.py
+++ code/Menu.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import pygame
 from pygame import Font, Surface, Rect
-
+g
 from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
 
 
@@ -19,8 +19,8 @@ class Menu:
 
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, 'Mountain', COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, 'Shooter', COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, 'Mountain', COLOR_WHITE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, 'Shooter', COLOR_WHITE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 self.menu_text2(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), (200 + 25 * i)))
