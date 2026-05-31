20ef285e759b3ffb6a009a081dbd35a7e3dbc4bc
Lázaro <lazaromessias101@gmail.com>
2026-05-31 10:45:34 -0300

    Adding classes generated from StarUML

---
+++ code/Menu.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pithon
+#-*- coding: utf-8 -*-
+
+class Menu:
+
+    def __init__(self):
+        self.window = None
+
+    def run(self):
+        pass
---
+++ code/Player.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pithon
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class Player(Entity):
+
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def move(self):
+        pass
